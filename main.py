ade451318f3d2800942dc53d70779c16f0620bc7
KethaapaAjaykumar <150053977+KethaapaAjaykumar@users.noreply.github.com>
2025-04-21 11:55:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,11 +20,18 @@ if __name__ == "__main__":
     security_manager = SecurityManager()
     client_api = ClientAPI(file_manager, replication_manager, metadata_manager, security_manager)
 
-    file_hash = client_api.upload_file("e1.txt", "HEllo i am ajay")
-    print(client_api.download_file("e1.txt", file_hash))
+    # Get input from the user
+    file_name = input("Enter the file name to upload (e.g., 'file.txt'): ")
+    file_content = input("Enter the file content: ")
 
+    # Upload and download the file
+    file_hash = client_api.upload_file(file_name, file_content)
+    downloaded_content = client_api.download_file(file_name, file_hash)
+
+    print("\nFile created successfully")
+    # print(downloaded_content) eneble this if you want to view content wtitten in that file 
+    # Fault detection and recovery
     failed_nodes = fault_tolerance_manager.detect_failures()
     fault_tolerance_manager.auto_recover(failed_nodes)
-
     server_thread = threading.Thread(target=run_server, daemon=True)
     server_thread.start()
